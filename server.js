00965fe90eeab3cdc1daabe43772119d323c4bf7
Yuyao Lin <linyuyaocd@outlook.com>
2025-12-11 15:26:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+// 设置静态文件夹，也就是前端文件存放的位置
+app.use(express.static(__dirname + '/public'));
 
-io.on('connection', socket => {
-  socket.on('draw', data => {
-    socket.broadcast.emit('draw', data);
+// 监听客户端连接
+io.on('connection', (socket) => {
+  console.log('有新用户连接: ' + socket.id);
+
+  // 监听 'drawing' 事件（当有人画画时）
+  socket.on('drawing', (data) => {
+    // 将画画的数据广播给除了发送者之外的所有人
+    socket.broadcast.emit('drawing', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('用户断开连接');
   });
 });
 
-server.listen(3000, () => console.log('Listening on http://localhost:3000'));
+http.listen(port, () => {
+  console.log('服务器运行在端口: ' + port);
+});
